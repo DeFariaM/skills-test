d26b8845c31875df50e206abdbca162715108a0d
MDeFaria <maria.defariasf@gmail.com>
2024-02-19 18:32:14 -0400

    add: routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" />
+        <Route path="/resources" />
+        <Route path="/pricing" />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
