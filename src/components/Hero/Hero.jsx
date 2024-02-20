fbc43f865bdf7507b81a501322b53a1580dd0021
MDeFaria <maria.defariasf@gmail.com>
2024-02-20 17:25:31 -0400

    add: question mark on input

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import lineal from "../../assets/lineargraphic.svg";
 import round from "../../assets/roundgraphic.svg";
+import QuestionMark from "../../icons/QuestionMark";
 
 const Hero = () => {
   return (
-    <section className=" h-fit">
+    <section id="hero" className=" h-fit">
       <div className=" h-[500px] w-full justify-center space-y-10 bg-brand-bg py-10 text-center md:h-[620px] md:rounded-3xl">
         <div className="space-y-6 md:mt-12">
-          <p className="justify-center text-4xl font-semibold leading-snug text-brand-primary md:text-7xl">
+          <h1 className="justify-center text-4xl font-semibold leading-snug text-brand-primary md:text-7xl">
             Grow your users.
             <br />
             <span className="text-brand-secondary">Smarter.</span>
-          </p>
+          </h1>
           <p className="mx-7 max-w-fit text-brand-secondary md:mx-auto md:max-w-[60%] ">
             Powerful, self-serve product and growth analytics to help you
             convert, engage, and retain more users. Trusted by over 4,000
@@ -19,13 +20,24 @@ const Hero = () => {
           </p>
         </div>
 
-        <div className="mx-auto flex w-[80%] flex-col space-y-5 text-left md:w-fit md:flex-row md:space-x-4 md:space-y-0">
+        <div
+          id="graphics"
+          className="mx-auto flex w-[80%] flex-col space-y-5 text-left md:w-fit md:flex-row md:space-x-4 md:space-y-0"
+        >
           <div className="flex flex-col space-y-2">
-            <input
-              type="email"
-              placeholder="Enter your email"
-              className=" h-fit w-[100%] rounded-md p-2 font-inter ring-inset focus:border-transparent  focus:ring-0 md:w-[30vw]"
-            />
+            <div className="relative">
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="relative h-fit w-[100%] rounded-md p-2 font-inter ring-inset  focus:border-transparent focus:ring-0 md:w-[30vw]"
+              />
+              <span
+                className="absolute right-2.5 top-2.5"
+                title="Enter your email"
+              >
+                <QuestionMark />
+              </span>
+            </div>
             <span className="text-xs font-light text-brand-primary">
               We care about your data in our{" "}
               <a href="" className="underline">
