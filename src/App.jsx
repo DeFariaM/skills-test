c20e59be93e4aa4fc900c9bda5b0c713fa57fe32
MDeFaria <maria.defariasf@gmail.com>
2024-02-20 17:22:02 -0400

    add: rutas complementarias y scroll into view para rutas hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home";
+import Footer from "./components/Footer/Footer";
+import { useEffect } from "react";
+import Products from "./pages/Products";
+import Company from "./pages/Company";
+import Resources from "./pages/Resources";
+import Pricing from "./pages/Pricing";
 
 function App() {
+  const { pathname, hash, key } = useLocation();
+
+  useEffect(() => {
+    if (hash === "") {
+      window.scrollTo(0, 0);
+    } else {
+      setTimeout(() => {
+        const id = hash.replace("#", "");
+        const element = document.getElementById(id);
+        if (element) {
+          element.scrollIntoView({ block: "center", behavior: "smooth" });
+        }
+      }, 0);
+    }
+  }, [pathname, hash, key]);
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/products" />
-        <Route path="/resources" />
-        <Route path="/pricing" />
+        <Route path="/products" element={<Products />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/company" element={<Company />} />
       </Routes>
+      <Footer />
     </>
   );
 }
